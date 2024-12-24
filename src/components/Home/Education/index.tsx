bc4eb9cba94f52b2c73cecb08f54011c92dc940a
Quang Thành <thanh.dangquang28@gmail.com>
2024-12-24 14:42:28 +0700

    refactor: update education content

--- src/components/Home/Education/index.tsx
+++ src/components/Home/Education/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 import { useContext } from 'react'
-import { Flex, List, Text } from '@mantine/core'
+import { Box, Flex, List, Text, ThemeIcon } from '@mantine/core'
 import { IconAward, IconBriefcase, IconCertificate } from '@tabler/icons-react'
 
 import ScrollFadeUp from '@/components/shared/scroll-fade-up'
@@ -57,78 +57,130 @@ const Education = () => {
               Oct 2020 - June 2024
             </Text>
           </Flex>
-          {/* <Text size='lg'>Ho Chi Minh City University of Technology (HCMUT)</Text> */}
-          <Text size='md'>Bachelor&apos;s degree, Computer Science</Text>
-          <Text size='md' visibleFrom='md'>
-            GPA: 8.05/10 (or 3.4/4)
-          </Text>{' '}
-          <Link href={''}>Transcript</Link>
+
+          <List listStyleType='disc' style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
+            <List.Item>
+              <Text size='md'>Bachelor&apos;s degree, Computer Science</Text>
+            </List.Item>
+            <List.Item>
+              <Box style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+                <Text size='md'>GPA: 8.05/10 (or 3.4/4)</Text>{' '}
+                <Link
+                  href={'https://drive.google.com/file/d/1URIBtR54R9uuouRMAvWEQezYutFzfKBY/view?usp=sharing'}
+                  target='_blank'
+                >
+                  <Text
+                    c='rgb(8, 205, 218)'
+                    style={{
+                      border: 'none',
+                      padding: 0,
+                      fontWeight: '500',
+                      textDecoration: 'underline',
+                    }}
+                  >
+                    Transcript
+                  </Text>
+                </Link>
+              </Box>
+            </List.Item>
+          </List>
+
+          {/* Achievement */}
           <Flex direction='column' gap='xs' mt={10}>
             <Text size='lg' fw={600}>
               Achievement
             </Text>
-            <List>
-              <List.Item icon={<IconAward size={16} />}>
-                <Text size='md'>Student with Five Good Merits Award</Text>
+            <List
+              icon={
+                <ThemeIcon size={24} radius='xl' variant='transparent' color='black'>
+                  <IconAward size={16} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
+                <Text size='md'>Student with Five Good Merits Award - Ho Chi Minh City Level</Text>
                 <Text size='sm' c='gray'>
-                  Issued by VNU-HCM & Ho Chi Minh City
+                  Issued by Vietnam Union of Students of HCMC
                 </Text>
               </List.Item>
-              <List.Item icon={<IconAward size={16} />}>
+              <List.Item>
                 <Text size='md'>Fossil Scholarship</Text>
                 <Text size='sm' c='gray'>
                   Issued by Fossil Group | EMPOWERING IT YOUTH
                 </Text>
               </List.Item>
-              <List.Item icon={<IconAward size={16} />}>
+              <List.Item>
                 <Text size='md'>CSE Alumni Scholarship</Text>
                 <Text size='sm' c='gray'>
                   Issued by Alumni of the CSE Faculty
                 </Text>
               </List.Item>
-              {/* <List.Item>
-                            <Text size='md'>Encouragement Scholarship <span style={{ color: 'gray' }}>| 1 Term</span> </Text>
-                        </List.Item> */}
             </List>
 
+            {/* Certificate */}
             <Text size='lg' fw={600}>
-              Certicicates
+              Certificates
             </Text>
-            <List>
-              <List.Item icon={<IconCertificate size={16} />}>
+            <List
+              icon={
+                <ThemeIcon size={24} radius='xl' variant='transparent' color='black'>
+                  <IconCertificate size={16} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
                 <Text size='md'>Software Development with Scrum Certificate</Text>
                 <Text size='sm' c='gray'>
                   Certified by Axon Active
                 </Text>
               </List.Item>
-              <List.Item icon={<IconCertificate size={16} />}>
+              <List.Item>
                 <Text size='md'>TOEIC Reading & Listening | Score: 680</Text>
               </List.Item>
             </List>
 
+            {/* Extracurricular Activity */}
             <Text size='lg' fw={600}>
               Extracurricular Activity
             </Text>
-            <List>
-              <List.Item icon={<IconBriefcase size={16} />}>
+            <List
+              icon={
+                <ThemeIcon size={24} radius='xl' variant='transparent' color='black'>
+                  <IconBriefcase size={16} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
                 <Text size='md'>CSE Multimedia</Text>
                 <Text size='sm' c='gray'>
                   Lead of design team: Design media publications and other products for events such as the CSE Job Fair,
-                  the Green Summer Volunteer Campaign, etc <Link href={''}>Explore my Gallery</Link>
+                  the Green Summer Volunteer Campaign, etc
+                  <Link
+                    href={'https://drive.google.com/file/d/1URIBtR54R9uuouRMAvWEQezYutFzfKBY/view?usp=sharing'}
+                    target='_blank'
+                  >
+                    <Text
+                      c='rgb(8, 205, 218)'
+                      style={{
+                        border: 'none',
+                        padding: 0,
+                        fontWeight: '500',
+                        textDecoration: 'underline',
+                      }}
+                    >
+                      Explore my Gallery
+                    </Text>
+                  </Link>
                 </Text>
               </List.Item>
-              <List.Item icon={<IconBriefcase size={16} />}>
+
+              <List.Item>
                 <Text size='md'>Spring Volunteer Campaign of CSE Faculty 2020 - 2021</Text>
                 <Text size='sm' c='gray'>
                   Volunteer: Support less fortunate people and primary students
                 </Text>
               </List.Item>
             </List>
-            {/* {
-                        experience.description.map((description, index) => (
-                            <Text key={index} size='md'>{description}</Text>
-                        ))
-                    } */}
           </Flex>
         </Flex>
       </Flex>
